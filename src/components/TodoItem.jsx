487c2e1c7b9eb38765966965c9e5407f509e447c
Adult96 <djxor007@gmail.com>
2023-02-11 18:05:51 +0900

    design: todo item style 구현 완료

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import theme from 'styles/theme';
 
 export default function TodoItem({ item }) {
   const { id, title, comment, isDone } = item;
   return (
-    <div>
+    <Item>
       <Link to={`/detail/${id}`}>상세보기</Link>
-      <p>{title}</p>
-      <p>{comment}</p>
-      <div>
-        <button>삭제하기</button>
-        <button>{isDone ? '취소!' : '완료!'}</button>
-      </div>
-    </div>
+      <Text fontSize='2rem' clamp={3}>
+        {title}
+      </Text>
+      <Text clamp={2}>{comment}</Text>
+      <BtnBox>
+        <Btn borderColor={theme.redPink}>삭제하기</Btn>
+        <Btn borderColor={theme.melonGreen}>{isDone ? '취소!' : '완료!'}</Btn>
+      </BtnBox>
+    </Item>
   );
 }
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 200px;
+  height: 300px;
+  margin: 1rem;
+  padding: 1rem;
+  border: 4px solid ${props => props.theme.darkMint};
+  border-radius: 0.5rem;
+  overflow: hidden;
+`;
+
+const Text = styled.div`
+  margin: 1rem;
+  font-size: ${props => props.fontSize || '1.5rem'};
+
+  word-break: break-all;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: ${props => props.clamp};
+  -webkit-box-orient: vertical;
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Btn = styled.button`
+  border: 2px solid ${props => props.borderColor};
+  border-radius: 0.5rem;
+  background-color: transparent;
+  font-size: 1.2rem;
+  padding: 0.5rem;
+`;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
 
 export default function TodoList({ todo, doneCheck, children }) {
   return (
-    <div>
+    <>
       {children}
-      {todo
-        .filter(value => value.isDone === doneCheck)
-        .map(item => (
-          <TodoItem key={uuidv4()} item={item} />
-        ))}
-    </div>
+      <List>
+        {todo
+          .filter(value => value.isDone === doneCheck)
+          .map(item => (
+            <TodoItem key={uuidv4()} item={item} />
+          ))}
+      </List>
+    </>
   );
 }
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
