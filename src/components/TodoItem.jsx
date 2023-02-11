2905e8f56ef63f7e4c12ee628c483e728a78d0e0
Adult96 <djxor007@gmail.com>
2023-02-11 16:29:57 +0900

    feat: todo item 기능 구현 완료

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function TodoItem({ item }) {
-  return <div></div>;
+  const { id, title, comment, isDone } = item;
+  return (
+    <div>
+      <Link to={`/detail/${id}`}>상세보기</Link>
+      <p>{title}</p>
+      <p>{comment}</p>
+      <div>
+        <button>삭제하기</button>
+        <button>{isDone ? '취소!' : '완료!'}</button>
+      </div>
+    </div>
+  );
 }
